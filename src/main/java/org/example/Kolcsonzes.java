ad1cbae8dab184968882a15b0ee248845f661e21
Zsolt Harmath <harmathzs@gmail.com>
2025-09-19 07:59:23 +0200

    Solve task 2

---
+++ src/main/java/org/example/Kolcsonzes.java
@@ -0,0 +1,58 @@
+package org.example;
+
+public class Kolcsonzes {
+    private String nev;
+    private char jarmu;
+    private int elvitelOra;
+    private int elvitelPerc;
+    private int visszaOra;
+    private int visszaPerc;
+
+    public String getNev() {
+        return nev;
+    }
+
+    public void setNev(String nev) {
+        this.nev = nev;
+    }
+
+    public char getJarmu() {
+        return jarmu;
+    }
+
+    public void setJarmu(char jarmu) {
+        this.jarmu = jarmu;
+    }
+
+    public int getElvitelOra() {
+        return elvitelOra;
+    }
+
+    public void setElvitelOra(int elvitelOra) {
+        this.elvitelOra = elvitelOra;
+    }
+
+    public int getElvitelPerc() {
+        return elvitelPerc;
+    }
+
+    public void setElvitelPerc(int elvitelPerc) {
+        this.elvitelPerc = elvitelPerc;
+    }
+
+    public int getVisszaOra() {
+        return visszaOra;
+    }
+
+    public void setVisszaOra(int visszaOra) {
+        this.visszaOra = visszaOra;
+    }
+
+    public int getVisszaPerc() {
+        return visszaPerc;
+    }
+
+    public void setVisszaPerc(int visszaPerc) {
+        this.visszaPerc = visszaPerc;
+    }
+}
